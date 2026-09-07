73ed701f42cfa95eed06240175f7245d1457d478
Ruben Alexandre Silva <66267648+srkzz@users.noreply.github.com>
2026-09-07 14:08:32 +0200

    Change MongoDB connection URI in app configuration
    Updated MongoDB connection URI with new credentials.

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 # --- Configuração da Aplicação ---
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', '000d88cd9d44446ebdd237eb6b0db000')
 app.config['MONGODB_SETTINGS'] = {
-    'host': os.getenv('MONGO_URI', "mongodb+srv://patrickribeiropt:zVMwTybneJ3WKBis@cluster0.jyjqmhv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    'host': os.getenv('MONGO_URI', "mongodb+srv://srkz:<Puta23Ideia0923*>@s1.bd9hrwc.mongodb.net/?appName=S1")
 }
 
 # Constante para o número de itens por página
